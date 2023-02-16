ace6b0d72388df95fdcca33e31cc4941ad09f209
BridgetACasey <bridgetannabelcasey@yahoo.com>
2023-02-16 14:18:23 +0000

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+project "ENTT"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "on"
+
+	targetdir ("build/" .. outputdir .. "/%{prj.name}")
+	objdir ("build-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.hpp",
+		"src/**.cpp"
+	}
+
+	includedirs
+	{
+		"src"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
